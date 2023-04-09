1f301fe6413346e59ebf1763319ba54573c66729
Oliver <olli143@live.de>
2023-04-09 11:11:09 +0200

    simplify Conditinals

--- src/Types/Conditionals.d.ts
+++ src/Types/Conditionals.d.ts
@@ -1,2 +1,2 @@
-export type If<T extends boolean, A, B = null> = T extends true ? A : T extends false ? B : A | B;
-export type ExplIf<T extends boolean, A, B = null> = T extends true ? A : T extends false ? B : B;
+export type If<T extends boolean, A, B = null> = T extends true ? A : B;
+export type ExplIf<T extends boolean, A, B = null> = T extends true ? A : B;
